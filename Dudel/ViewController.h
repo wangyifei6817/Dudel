a2b051ef54692fd97e806f4f2c881e51cabceea4
王一飞 <wangyifei6817@gmail.com>
2013-04-02 10:29:34 +0800

    Initial Commit

---
+++ Dudel/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Dudel
+//
+//  Created by wangyifei on 13-4-2.
+//  Copyright (c) 2013年 Me. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Dudel/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Dudel
+//
+//  Created by wangyifei on 13-4-2.
+//  Copyright (c) 2013年 Me. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
